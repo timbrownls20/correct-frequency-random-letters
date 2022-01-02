37d6fa6a979580b2ada3fec10cf1ed8e74689146
timbrownls20 <timbrownls26@hotmail.com>
2022-01-02 16:54:41 +1000

    tests for text frequencies

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { DictionaryFrequency, IRandomNumberGenerator } from ".";
+import { DictionaryFrequency, TextFrequency, IRandomNumberGenerator } from ".";
 
 class MockRandom implements IRandomNumberGenerator {
   Random(): number {
@@ -27,8 +27,8 @@ describe("Dictionary frequencies", () => {
     expect(letter).toBe("Z");
   });
 
-  test("cumulative frequency 8 should be B", () => {
-    const textFrequencies = new DictionaryFrequency(new MockRandom(8));
+  test("cumulative frequency 12 should be B", () => {
+    const textFrequencies = new DictionaryFrequency(new MockRandom(12));
     const letter = textFrequencies.random();
     expect(letter).toBe("B");
   });
@@ -39,3 +39,36 @@ describe("Dictionary frequencies", () => {
     expect(letter).toBe("R");
   });
 });
+
+describe("Text frequencies", () => {
+    test("should be a letter", () => {
+      const textFrequencies = new TextFrequency();
+      const letter = textFrequencies.random();
+      expect(letter).toMatch(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/);
+    });
+  
+    test("cumulative frequency 0 should be A", () => {
+      const textFrequencies = new TextFrequency(new MockRandom(0));
+      const letter = textFrequencies.random();
+      expect(letter).toBe("A");
+    });
+  
+    test("cumulative frequency 100 should be Z", () => {
+      const textFrequencies = new TextFrequency(new MockRandom(100));
+      const letter = textFrequencies.random();
+      expect(letter).toBe("Z");
+    });
+  
+    test("cumulative frequency 12 should be B", () => {
+      const textFrequencies = new TextFrequency(new MockRandom(12));
+      const letter = textFrequencies.random();
+      expect(letter).toBe("B");
+    });
+  
+    test("cumulative frequency 72 should be S", () => {
+      const textFrequencies = new TextFrequency(new MockRandom(72));
+      const letter = textFrequencies.random();
+      expect(letter).toBe("S");
+    });
+  });
+  
